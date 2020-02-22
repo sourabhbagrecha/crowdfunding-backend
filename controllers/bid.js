ded5fe37b8172b7c5fa65902553e9605b7fc37ae
Sourabh Bagrecha <sourabhbagrecha@gmail.com>
2020-02-22 16:23:43 +0530

    edit bid feature

--- controllers/bid.js
+++ controllers/bid.js
@@ -4,7 +4,7 @@ const postBid = async (req, res, next) => {
   try {
     const {userId} = req;
     const {projectId} = req.params;
-    const {amount} = req.body;
+    const amount = parseInt(req.body.amount);
     const user = await User.findById(userId);
     if(!user){
       return res.status(400).json({msg: "No such user exists!"});      
@@ -40,4 +40,51 @@ const fetchMyBids = async (req, res, next) => {
   }
 }
 
-module.exports = {postBid, fetchMyBids};
+const editBid = async (req, res, next) => {
+  try {
+    const {userId} = req;
+    const {bidId} = req.params;
+    const amount = parseInt(req.body.amount);
+    const user = await User.findById(userId);
+    const bid = await Bid.findById(bidId);
+    console.log({bidId})
+    const project = await Project.findById(bid.project).select('funds bids');
+    if(!user){
+      return res.status(400).json({msg: "No such user exists!"});      
+    }
+    if((user.balance + bid.amount) < amount){
+      return res.status(400).json({msg: "Insufficient Funds!"});
+    }
+    user.balance += bid.amount;
+    user.balance -= amount;
+    const updatedUser = await user.save();
+
+    project.funds = parseInt(project.funds);
+    project.funds -= bid.amount;
+    project.funds += amount;
+    const updatedProject = await project.save();
+    
+    
+    bid.amount = amount;
+    const updatedBid = await bid.save();
+    
+    return res.status(201).json({bid, project, balance: user.balance});  
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}
+
+const getBid = async (req, res, next) => {
+  try {
+    const {bidId} = req.params;
+    const {userId} = req;
+    const bid = await Bid.findById(bidId).populate("project");
+    if(userId.toString() !== bid.user.toString()) return res.status(400).json({msg: "Not authenticated!"});
+    return res.status(200).json({bid});
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {postBid, fetchMyBids, editBid, getBid};
--- routes/api/bid.js
+++ routes/api/bid.js
@@ -8,7 +8,9 @@ const Bid = require("../../models/bid");
 const isAuthenticated = require("../../middlewares/isAuth");
 const bidController = require("../../controllers/bid");
 
+router.post("/edit/:bidId", isAuthenticated, bidController.editBid);
 router.post("/:projectId", isAuthenticated, bidController.postBid);
 router.get("/fetch-all", isAuthenticated, bidController.fetchMyBids);
+router.get("/:bidId", isAuthenticated, bidController.getBid);
 
 module.exports = router;
